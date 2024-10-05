b1ed77ef4180feaed372e921a5cf6f90e64f377b
Azner01 <azneraparicio@hotmail.com>
2024-10-05 17:59:02 -0500

    Funcion para mostrar el grafico de linea entre hombres y mujeres

--- src/modules/dataAnalysis/components/MultiLine.jsx
+++ src/modules/dataAnalysis/components/MultiLine.jsx
@@ -21,39 +21,75 @@ defaults.plugins.title.align = "start";
 defaults.plugins.title.font.size = 20;
 defaults.plugins.title.color = "black";
 
+
+
 function LineGraph(){
-  return (
-    <div>
-      <h1>Mutli Líneas</h1>
-      <Line 
-      data = {{
-        labels: OlimpycData.map((data)=> data.Year),
+  const groupedData = {};
+  OlimpycData.forEach(athlete => {
+    const year = athlete.Year;
+    const gender = athlete.Gender.toLowerCase();
+    if (!groupedData[year]) {
+      groupedData[year] = { men: 0, women: 0 };
+    }
+
+    groupedData[year][gender]++;
+  });
+  // Crear los conjuntos de datos para Chart.js
+  const years = Object.keys(groupedData);
+  const menData = years.map(year => groupedData[year].men);
+  const womenData = years.map(year => groupedData[year].women);
+  
+  const chartData = {
+    labels: OlimpycData.map((data)=> data.Year),
         datasets: [
           {
             label: "Hombres",
-            data: OlimpycData.map((data)=> data.Gender == "Men"),
+            data: menData,
             backgroundColor: "#064FF0",
             borderColor: "#064FF0",
+            fill: false,
+            tension: 0.4,
+            pointRadius: 3,
           },
           {
             label: "Mujeres",
-            data: OlimpycData.map((data)=> data.Gender == "Women"),
+            data: womenData,
             backgroundColor: "#FF3030",
             borderColor: "#FF3030",
+            fill: false,
+            tension: 0.4,
+            pointRadius: 3,
           }
         ],
-      }}
+  }
+
+
+  return (
+    <div>
+      <h1>Multi Líneas</h1>
+      <h2>{}</h2>
+      <Line 
+      data = {chartData}
       option = {{
-        elements: {
-          line: {
-            tension: 0.5,
-          },
+        responsive: true,
+        title: {
+          display: true,
+          text: 'Participación de hombres y mujeres en las Olimpiadas'
         },
-        plugins: {
-          title: {
-            text: "Hombres vs Mujeres en Olimpiadas"
+          scales: {
+            xAxes: [{
+              scaleLabel: {
+                display: true,
+                labelString: 'Año'
+              }
+            }],
+            yAxes: [{
+              scaleLabel: {
+                display: true,
+                labelString: 'Número de atletas'
+              }
+            }]
           }
-        }
       }}
       />
     </div>
